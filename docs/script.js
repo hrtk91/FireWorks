705fe96a5dd2e16ead3c0e8b898ed7ce81a488d0
hrtk91 <ht91218@gmail.com>
2018-09-10 22:13:14 +0900

    拡散弾追加

--- docs/script.js
+++ docs/script.js
@@ -5,7 +5,7 @@ var ASSETS = {
     'se1': './se/fireworks1.mp3',
     'se2': './se/fireworks2.mp3',
     'se3': './se/fireworks3.mp3',
-    'bgm': './bgm/bgm.mp3'
+    //'bgm': './bgm/bgm.mp3'
   }
 };
 
@@ -22,12 +22,19 @@ phina.define('MainScene', {
     //SoundManager.playMusic('bgm');
   },
   update: function (app) {
-    if (app.frame % 30 === 0) {
-      var p = FireWorks({
+    if (app.frame % 60 === 0) {
+      var fireworks = FireWorks;
+      /*
+      if (app.frame % 2 === 0)
+        fireworks = FireWorks;
+      else if (app.frame % 3 === 0)
+        fireworks = Cluster;
+      */
+      var p = fireworks({
         strokeWidth: 0,
         fill: 'hsla({0}, 100%, 60%, 1)'.format(Math.randint(0, 360)),
         radius: 2,
-        flowerRange: Math.random() + 1,
+        flowerRadius: Math.random() + 1,
       }).addChildTo(this);
       p.blendMode = 'lighter';
       p.setPosition(Math.randint(0, 600), 960);
@@ -43,12 +50,12 @@ phina.define('MainScene', {
 
 phina.define('Particle', {
   superClass: 'CircleShape',
-  
   init: function (option) {
     option = option || {};
     this.superInit(option);
+    this.v = Vector2(0, 0);
     this.blendMode = 'lighter';
-    this.life = this.maxlife = option.life || Math.randint(60, 100);
+    this.life = this.maxlife = option.life || 50;
     this.friction = option.friction || 0.987;
     this.on('tod', this.remove);
   },
@@ -59,8 +66,7 @@ phina.define('Particle', {
   update: function (app) {
     this.position.add(this.v);
     this.v.mul(this.friction);
-    //this.v.y += 0.01;
-    this.alpha = (this.life) / this.maxlife;
+    this.alpha = (this.life / this.maxlife) * this.v.length();
     
     if (app.frame % 5 === 0)
       this.life--;
@@ -75,56 +81,100 @@ phina.define('FireWorks', {
   init: function (option) {
     option = option || {};
     this.superInit(option);
-    this.superUpdate = Particle().update;
-    this.flowerRange = option.flowerRange || 1.5;
-    this.threshold = option.threshold || -1;
+    this.flowerRadius = option.flowerRadius || 1.5;
     this.flowerNum = option.flowerNum || 30;
   },
-  exterior: function (num) {
+  outer: function (outers) {
     var self = this;
-    const flowersNum = num;
-    const baserad = (2 * Math.PI) / flowersNum;
-    (flowersNum).times(function (i) {
-      var p = Particle({
-        fill: self.fill,
-        radius: 3,
-        strokeWidth: self.strokeWidth,
-      }).addChildTo(self.parent);
+    const baserad = (2 * Math.PI) / outers.length;
+    outers.forEach(function (p, i, ary) {
+      p.addChildTo(self.parent);
       p.setPosition(self.x, self.y);
       p.v = Vector2(Math.cos(i * baserad), Math.sin(i * baserad));
-      p.v.mul(self.flowerRange);
-      p.life = 50;
+      p.v.normalize();
+      p.v.mul(self.flowerRadius);
+      p.fill = self.fill;
     });
   },
-  interior: function (num) {
-    var self = this;
-    const flowersNum = num;
-    const baserad = (2 * Math.PI) / flowersNum;
-    (flowersNum).times(function (i) {
+  createOuters: function (option) {
+    option = option || {};
+    const outers = [];
+    const flowersNum = option.num;
+    for (var i = 0; i < flowersNum; i++) {
       var p = Particle({
-        fill: self.fill,
-        radius: 5,
-        strokeWidth: self.strokeWidth,
-      }).addChildTo(self.parent);
+        fill: option.fill || 'Silver',
+        radius: option.radius || 3,
+        strokeWidth: option.strokeWidth || 0,
+      });
+      outers.push(p);
+    }
+    return outers;
+  },
+  inner: function (inners) {
+    var self = this;
+    const baserad = (2 * Math.PI) / inners.length;
+    inners.forEach(function (p, i, ary) {
+      p.addChildTo(self.parent);
       p.setPosition(self.x, self.y);
       p.v = Vector2(Math.cos(i * baserad), Math.sin(i * baserad));
-      p.v.mul(Math.random()*self.flowerRange);
-      p.life = 50;
+      p.v.normalize();
+      p.v.mul(Math.random() * self.flowerRadius);
+      p.fill = self.fill;
     });
   },
-  explosion: function (app) {
-    const flowersNum = this.flowerNum;
-    this.exterior(flowersNum / 4);
-    this.interior(flowersNum);
-    this.remove();
+  createInners: function (option) {
+    option = option || {};
+    const flowerNum = option.num;
+    const inners = [];
+    for (var i = 0; i < flowerNum; i++) {
+      var p = Particle({
+        fill: option.fill || 'Gold',
+        radius: option.radius || 5,
+        strokeWidth: option.strokeWidth || 0,
+      });
+      inners.push(p);
+    }
+    return inners;
+  },
+  explosion: function () {
+    const flowerNum = this.flowerNum;
+    const outers = this.createOuters({num:flowerNum/2});
+    const inners = this.createInners({num:flowerNum});
+    this.outer(outers);
+    this.inner(inners);
   },
   update: function (app) {
-    this.superUpdate(app);
+    Particle.prototype.update.call(this, app);
     
-    if (this.life <= 0 || this.v.y >= this.threshold) {
-      this.explosion(app);
+    if (this.v.length() < 1) {
+      this.explosion();
       SoundManager.play('se' + Math.randint(1, 2).toString());
+      this.remove();
     }
+  },
+});
+
+phina.define('Cluster', {
+  superClass: 'FireWorks',
+  init: function (option) {
+    option = option || {};
+    this.superInit(option);
+  },
+  createInners: function (option) {
+    option = option || {};
+    const inners = [];
+    const flowerNum = 5;
+    for (var i = 0; i < flowerNum; i++) {
+      var p = FireWorks({
+        fill: option.fill,
+        radius: option.radius || 5,
+        strokeWidth: option.strokeWidth || 0,
+        flowerNum: flowerNum,
+        flowerRadius: 1,
+      });
+      inners.push(p);
+    }
+    return inners;
   },
 });
 
